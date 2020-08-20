593cf12bbec4278b11a32414980bdb2df5eb9bf3
Pandance <mangotiger1998@gmail.com>
2020-08-20 05:22:43 -0700

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+def main():
+  method1("hello world")
+  method1("how on earth does syntax work again??")
+
+
+def method1(words):
+  print("Me says " + words)
+
+
+if __name__ == "__main__":
+    main()
